936690c6ed7945e05a7c49909cc6003e5036c39b
life0001 <life0001@126.com>
2017-03-26 11:12:19 +0800

    随机数字计算器

--- counter.js
+++ counter.js
@@ -6,19 +6,18 @@
         numArry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'];
 
     // 输出随机数字
-    function getRandomNum() {
+    (function() {
         for (var i = 0, len = $num.length; i < len; i++) {
             var index = Math.floor(Math.random() * numArry.length);
             $num.eq(i).text(numArry[index]);
             numArry.splice(index, 1);
         }
-    }
-
-    getRandomNum();
+    })();
 
     // 鼠标点击事件
     $('.counterContent').on('click', 'a', function () {
-        switch ($(this).text()) {
+        var txt = $(this).text();
+        switch (txt) {
             case 'C':
                 $procedure.text(0);
                 break;
@@ -44,7 +43,6 @@
                 isRepeat = true;
                 break;
             default :
-                var txt = $(this).text();
                 if (txt == '+' || txt == '-' || txt == '*' || txt == '/') {
                     if (!isRepeat) break;
                     $procedure.text() == 0 && $procedure.text('');
